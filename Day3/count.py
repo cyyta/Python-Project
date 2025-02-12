18e2ccae321bfe27bd3c1f8cdfc3f1740b538e9b
Cyyta <be.cyyta@gmail.com>
2025-02-12 20:14:36 +0545

    learning

--- Day3/count.py
+++ Day3/count.py
@@ -1,2 +1,15 @@
-count = [1,2,3,4,5,6]
-print(f"")
+#count = [1,2,3,4,5,6]
+#print(f"")
+
+#new_list =[]
+#for i in range(0,5):
+ #   user_input = input("enter num")
+  #  new_list.append(user_input)
+   # print(new_list)
+#print(new_list)
+
+#num_list = input("Enter a list of numbers separated by spaces: ").split()
+#num_list = [int(num) for num in num_list] #one for loop
+
+
+#PRIME NUMBEr
