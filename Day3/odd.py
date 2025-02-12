3cac659655d731d931612e0429915baf898ac963
Cyyta <be.cyyta@gmail.com>
2025-02-12 19:49:30 +0545

    odd and even check

---
+++ Day3/odd.py
@@ -0,0 +1,8 @@
+#program to input numbers in list and check for odd or even
+list = [1,2,3,4,5,6,7,8,9,11]
+for i in list:
+    if(i%2==0):
+        print(f"Even numbers are:\n {i}")
+    else:
+        print(f"Odd numbers are:\n {i}")
+    
