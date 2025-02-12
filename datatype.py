a36e4a4177cfe931cf0227c4987ebe5d91a395c3
Cyyta <be.cyyta@gmail.com>
2025-02-12 18:00:26 +0545

    DataTypes of Python

---
+++ datatype.py
@@ -0,0 +1,29 @@
+#DataTypes are the classification or categorization of data items...
+# simply a variable stores different type of data
+
+
+
+#integer
+number = 1234
+print(number)
+print(type(number))
+
+#Float
+num = 11.11
+print(num)
+print(type(num))
+
+#String 
+name = "Shitoshna Basnet"
+print(name)
+print(type(name))
+
+#Boolean
+
+value = True
+print(value)
+print(type(value))
+
+
+
+ 
