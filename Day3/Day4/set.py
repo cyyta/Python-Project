847dc621b31648f1bd760db013eec4b977e06552
Cyyta <be.cyyta@gmail.com>
2025-02-14 08:24:35 +0545

    inheritance in python

--- Day3/Day4/set.py
+++ Day3/Day4/set.py
@@ -1,5 +1,27 @@
 #set unordered collection of unique element
 #not repeat element
 
-first_set = {"aa",1,1,2,2,4,5,7,7,"sita"}
-print(first_set)
+#first_set = {"aa",1,1,2,2,4,5,7,7,"sita"}
+#print(first_set)
+
+
+class Animal:
+
+ def __init__(self,name):
+    self.name = name 
+
+ def displyMethod(self):
+    print(self.name)
+class Dog(Animal):  #dog inherit animal    
+ def __init__(self,name): 
+    super().__init__(name)
+    super().displyMethod()
+ def speak(self):
+    print(f"{self.name} barks") 
+
+dog_one = Dog("Dog 1")
+dog_2 = Dog("Dog  2")
+dog_one.displyMethod()
+dog_2.speak()     
+
+
